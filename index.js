5e6cef234315c471b1bb4215ec0901afc6192f23
Vince Elizaga <vncelizaga@gmail.com>
2017-09-04 05:20:36 +0800

    fix: router and refresh bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express  = require('express');
 var app      = express();                               // create our app w/ express
 var morgan = require('morgan');             // log requests to the console (express4)
+var path  = require('path');
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var port = Number(process.env.PORT || 8000);
 // configuration =================
@@ -10,6 +11,13 @@ app.use(morgan('dev'));                                         // log every req
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+
+var renderIndex = function(req, res){
+    console.log(__dirname);
+    res.sendFile(path.resolve(__dirname, __dirname + '/dist/index.html'));
+}
+
+app.get('/*', renderIndex);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
